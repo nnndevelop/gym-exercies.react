daba0d535d29cd5d24e9ef35d88dfad40c6d354b
Nurbek <nurillavnurbek38@gmail.com>
2022-08-08 22:58:54 +0500

    commit

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key':'712793bc70msh62cb6893c78189fp1be112jsn9c3d7e3aa5d6',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': '712793bc70msh62cb6893c78189fp1be112jsn9c3d7e3aa5d6',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
